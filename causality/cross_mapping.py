7a404d4a35f67615a81b7f403491a59d7635784a
Giulio Tirabassi <tirabassi.giulio@gmail.com>
2023-04-26 17:15:52 +0200

    Add partial cross mapping

--- causality/cross_mapping.py
+++ causality/cross_mapping.py
@@ -24,12 +24,73 @@ def convergent_cross_mapping(y, x, dim=2, lag=1):
     x = np.array(x)[::-1]  # future in the beginning past in the end
     y = np.array(y)[::-1]
     assert x.size == y.size
+    y_hat = _compute_cross_mapping(x, y, dim, lag)
+    return pearsonr(y[: y_hat.size], y_hat)
+
+
+def _compute_cross_mapping(x, y, dim, lag):
     X = time_delay_embedding(x, dim=dim, lag=lag)
     neigh = NearestNeighbors(n_neighbors=dim + 1, radius=np.inf).fit(X)
     distances, neigh_index = neigh.kneighbors(return_distance=True)
-    distances = distances[:, 1:]
-    neigh_index = neigh_index[:, 1:]
     u = np.exp(-distances.T / distances[:, 0]).T
     w = (u.T / np.sum(u, axis=1)).T
     y_hat = (w * np.take(y, neigh_index)).sum(axis=1)
-    return pearsonr(y[: y_hat.size], y_hat)
+    return y_hat
+
+
+def partial_cross_mapping(x, y, z, dim, maxlag):
+    zhat = _find_most_correlated_mapping(x, z, dim, maxlag)
+    yhathat = _find_most_correlated_mapping(zhat, y, dim, maxlag)
+    yhat = _find_most_correlated_mapping(x, y, dim, maxlag)
+    return _partial_cross_correlation(y, yhat, yhathat)
+
+
+def _corr(x, y):
+    length = min(x.size, y.size)
+    return pearsonr(x[:length], y[:length])[0]
+
+
+def _partial_cross_correlation(x, y, z):
+    return (_corr(x, y) - _corr(x, z) * _corr(y, z)) / np.sqrt(
+        (1 - _corr(x, z) ** 2) * (1 - _corr(y, z) ** 2)
+    )
+
+
+def _find_most_correlated_mapping(x, z, dim, maxlag):
+    """Most correlated mapping Z --> X"""
+    zhat = None
+    corr_zhat = 0
+    for lag in range(1, maxlag):
+        zhatdummy = _compute_cross_mapping(x, z, dim=dim, lag=lag)
+        corr, _ = pearsonr(z[: zhatdummy.size], zhatdummy)
+        if abs(corr) > corr_zhat:
+            corr_zhat = abs(corr)
+            zhat = zhatdummy
+    return zhat
+
+
+if __name__ == "__main__":
+    rx = 3.6
+    ry = 3.72
+    rz = 3.68
+    betayz = 0.35
+    betazx = 0.35
+    maxL = 1000
+    X = np.zeros(maxL)
+    Y = np.zeros(maxL)
+    Z = np.zeros(maxL)
+    X[0] = 0.4
+    Y[0] = 0.4
+    Z[0] = 0.4
+    for j in range(1, maxL):
+        X[j] = X[j - 1] * (rx - rx * X[j - 1]) + np.random.normal(0, 0.005)
+        Y[j] = Y[j - 1] * (ry - ry * Y[j - 1] - betayz * Z[j - 1]) + np.random.normal(
+            0, 0.005
+        )
+        Z[j] = Z[j - 1] * (rz - rz * Z[j - 1] - betazx * X[j - 1]) + np.random.normal(
+            0, 0.005
+        )
+    E = 4
+    tau = 1
+    print(partial_cross_mapping(Y, X, Z, dim=E, maxlag=10))
+    print(convergent_cross_mapping(Y, X, dim=E, lag=1))
